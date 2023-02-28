87e5a1bcec91085c805842b4df04a9e0d36bdbac
GurpreetRandhawa <singhpreet01991@gmail.com>
2023-02-28 12:04:50 -0800

    add test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,26 @@
 const request = require("request");
-const breed = process.argv[2];
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
-  (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      const data = JSON.parse(body);
-      if (data.length > 0) {
-        console.log(data[0].description);
-        console.log(typeof data);
-      } else {
-        console.log("Breed not found");
+const fetchBreedDescription = (breedName, callback) => {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        callback(error);
+      }
+      // else if (response && response.statusCode === 200) {
+      //   const data = JSON.parse(body);
+      //   console.log(data[0].description);
+      // } else {
+      //   console.log("Breed not found");
+      // }
+      else {
+        const data = JSON.parse(body);
+        if (data.length > 0) {
+          callback(error, data[0].description);
+        } else {
+          callback(error, "Breed not found");
+        }
       }
     }
-  }
-);
+  );
+};
+module.exports = { fetchBreedDescription };
