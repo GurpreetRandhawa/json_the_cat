82a0c9fe48f294f6b9fd0496d86fff669b7f7641
GurpreetRandhawa <singhpreet01991@gmail.com>
2023-02-28 13:57:38 -0800

    refactor breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,15 +4,8 @@ const fetchBreedDescription = (breedName, callback) => {
     `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
     (error, response, body) => {
       if (error) {
-        callback(error);
-      }
-      // else if (response && response.statusCode === 200) {
-      //   const data = JSON.parse(body);
-      //   console.log(data[0].description);
-      // } else {
-      //   console.log("Breed not found");
-      // }
-      else {
+        callback(error, null);
+      } else {
         const data = JSON.parse(body);
         if (data.length > 0) {
           callback(error, data[0].description);
